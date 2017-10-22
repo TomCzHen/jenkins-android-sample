954d38caceca559aff957fa3f19d654ede711fb9
Tom CzHen <tom.czhen@gmail.com>
2017-10-22 22:55:18 +0800

    test SECRET_KEY

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -13,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String message = getResources().getString(R.string.app_name);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(BuildConfig.SECRET_KEY);
     }
 
     public void sendMessage(View view) {
